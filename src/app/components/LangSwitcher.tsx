8bac94fb0646981ef7d5245e33afb3aaca8c6251
pkovvk <popkovkirill1904@gmail.com>
2025-02-15 23:28:45 +1100

    Bug fixes

--- src/app/components/LangSwitcher.tsx
+++ src/app/components/LangSwitcher.tsx
@@ -11,14 +11,25 @@ export default function LangSwitcher({
   setLang: React.Dispatch<React.SetStateAction<LanguageData>>;
 }) {
   const changeLang = (language: string) => {
-    const newLang = language === 'ru' ? locales.lang.ru : locales.lang.en;
+    const newLang = language === "ru" ? locales.lang.ru : locales.lang.en;
     setLang(newLang);
+    localStorage.setItem("lang", language);
   };
 
   return (
     <div className="lang-switcher">
-      <button className={lang === locales.lang.ru ? "active" : ""} onClick={() => changeLang('ru')}>RU</button>
-      <button className={lang === locales.lang.en ? "active" : ""} onClick={() => changeLang('en')}>EN</button>
+      <button
+        className={lang === locales.lang.ru ? "active" : ""}
+        onClick={() => changeLang("ru")}
+      >
+        RU
+      </button>
+      <button
+        className={lang === locales.lang.en ? "active" : ""}
+        onClick={() => changeLang("en")}
+      >
+        EN
+      </button>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import locales from "@/app/locales/locales.json";
 import BackgroundVideo from "@/app/components/Video";
 import Header from "@/app/components/Header";
 
 export default function Home() {
   const [lang, setLang] = useState(locales.lang.ru);
+
+  useEffect(() => {
+    const language = localStorage.getItem("lang");
+    if (language) {
+      const newLang = language === "ru" ? locales.lang.ru : locales.lang.en;
+      setLang(newLang);
+    }
+  });
 
   return (
     <>
